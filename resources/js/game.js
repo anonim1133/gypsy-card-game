623becb29937cd4268c7b654e1464f90e02cc7fc
Krzysztof Oleśków <KOleskow@si-consulting.pl>
2015-09-07 12:14:51 +0200

    Adding user to players list after joining

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,7 +1,17 @@
 function joinGame(gid, uid){
     $.get( "/joinGame/" + gid + "/" + uid, function( data ) {
-		console.log(gid);
-        console.log(data);
+		var list = $('#player_list');
+		var new_player = $('<li>').attr('id', data.uid).text(data.uname);
+
+		var found = false;
+		$('#player_list li').each(function(key, li){
+
+			if($(li).attr('id') == data.uid) found = true;
+		});
+
+
+		if(!found) list.append(new_player);
+		
     }).fail(function(){
         console.log('Error while joining game');
     });
